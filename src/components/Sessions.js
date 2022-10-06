d57a7efb4634b60e4a3696433555d3a734d7d425
brenokamura <96897837+brenokamura@users.noreply.github.com>
2022-10-06 00:45:29 -0300

    Update Sessions.js

--- src/components/Sessions.js
+++ src/components/Sessions.js
@@ -35,9 +35,24 @@ export default function Sessions(){
 
 const TitleContainer = styled.div
 `
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 30px;
+  width: 100%;
+  margin-top: 25px;
+  text-align: center;
+  font-size: 24px;
 
 `
 const SessionsContainer = styled.div
 `
-
+display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 10vh auto;
+  margin-bottom: 14vh;
+  width: 100%;
+  background-color: #ffffff;
 `
