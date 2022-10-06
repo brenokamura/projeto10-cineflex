1ec4fccdaeadd4b8676f306fa5a587dda51d5bd4
brenokamura <96897837+brenokamura@users.noreply.github.com>
2022-10-06 00:52:59 -0300

    Update Session.js

--- src/components/Session.js
+++ src/components/Session.js
@@ -1,35 +1,48 @@
 import styled from "styled-components";
 
-function Session({ diaSemana, data , sessoes}) {
-    return (
-        <>
-
-            <SessionList>
-                <SessionDay>{diaSemana} - {data}</SessionDay>
-                <SessionTime>{sessoes}</SessionTime>
-            </SessionList>
-
-        </>
-    );
+function Session({ weekday, date, showtimes }) {
+  return (
+    <Container>
+      {`${weekday} - ${date}`}
+      <Buttons>
+        {showtimes.map((time) => (
+          <Button key={time.id}>
+            {time.name}
+          </Button>
+        ))}
+      </Buttons>
+    </Container>
+  );
 }
 
-export default Session;
 
-const SessionList = styled.div`
-    display: flex;
-    height: 193px;
-    width: 129px;
-    left: 38px;
-    top: 177px;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 10px;
+  font-size: 20px;
+  box-sizing: border-box;
+`;
 
-`
+const Buttons = styled.div`
+  display: flex;
+  justify-content: left;
+  margin-top: 20px;
+  align-items: center;
+`;
 
-const SessionDay = styled.div
-`
-
-`
-
-const SessionTime = styled.div
-`
-
-`
+const Button = styled.div`
+  display: flex;
+  height: 32px;
+  width: 64px;
+  justify-content: center;
+  align-items: center;
+  margin: 2px 8px 8px 0;
+  border-radius: 2px;
+  background-color: #e8833a;
+  font-size: 18px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
